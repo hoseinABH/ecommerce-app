76786fc3cc8c349053907d6d41cf86f21d37f3ad
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2022-01-24 16:50:50 +0330

    add dynamic config for each framework

---
+++ framework/common/config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const merge = require('deepmerge');
+const { writeFileSync } = require('fs');
+
+function withFrameworkConfig(defaultConfig = {}) {
+  const framework = 'shopify';
+
+  const frameworkNextConfig = require(path.join(
+    '../',
+    framework,
+    'next.config'
+  ));
+
+  const config = merge(defaultConfig, frameworkNextConfig);
+
+  const tsPath = path.join(process.cwd(), 'tsconfig.json');
+  const tsConfig = require(tsPath);
+
+  tsConfig.compilerOptions.paths['@framework'] = [`framework/${framework}`];
+  tsConfig.compilerOptions.paths['@framework/*'] = [`framework/${framework}/*`];
+  writeFileSync(tsPath, JSON.stringify(tsConfig, null, 2));
+
+  return config;
+}
+
+module.exports = { withFrameworkConfig };
--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
+const { withFrameworkConfig } = require('./framework/common/config');
+
+module.exports = withFrameworkConfig({
   reactStrictMode: true,
-}
+
+  i18n: {
+    locales: ['en-US', 'es'],
+    defaultLocale: 'en-US',
+  },
+});

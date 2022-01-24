5a6448a8f100482a7cb9280499386fe74ff35396
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2022-01-24 10:22:15 +0330

    add get product query

---
+++ framework/shopify/product/get-all-products.ts
@@ -0,0 +1,9 @@
+import { fetchApi } from '../utils/fetch-api';
+import getAllProductsQuery from '../utils/queries/get-all-products';
+
+const getAllProducts = async (): Promise<any[]> => {
+  const products = await fetchApi({ query: getAllProductsQuery });
+  return products.data;
+};
+
+export default getAllProducts;

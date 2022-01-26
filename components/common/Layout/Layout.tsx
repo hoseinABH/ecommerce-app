1337da5e3d50c90e1bff952a91e5c9257c119782
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2022-01-26 13:13:04 +0330

    add <Navbar /> component

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,10 +1,13 @@
 import { FC } from 'react';
 import Footer from '../Footer';
+import Navbar from '../Navbar';
 import styles from './Layout.module.scss';
 
 const Layout: FC = ({ children }) => {
   return (
     <div className={styles.root}>
+      <Navbar />
+
       <main className="fit">{children}</main>
 
       <Footer />
---
+++ components/common/Navbar/Navbar.tsx
@@ -0,0 +1,39 @@
+import { FC } from 'react';
+import Link from 'next/link';
+import { Container } from '@components/ui';
+import styles from './Navbar.module.scss';
+
+interface IProps {}
+
+const Navbar: FC<IProps> = () => {
+  return (
+    <Container>
+      <div className="flex md:py-6">
+        <div className="flex flex-1 items-center">
+          <Link href="/">
+            <a className="text-xl font-bold">NEXT_STORE</a>
+          </Link>
+          <nav className="ml-6 space-x-6">
+            <Link href="/">
+              <a className={styles.link}>All</a>
+            </Link>
+            <Link href="/">
+              <a className={styles.link}>Clothes</a>
+            </Link>
+            <Link href="/">
+              <a className={styles.link}>Accesories</a>
+            </Link>
+            <Link href="/">
+              <a className={styles.link}>Shoes</a>
+            </Link>
+          </nav>
+          {/* <div className="flex flex-1 justify-end space-x-8">
+            <Usernav />
+          </div> */}
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Navbar;

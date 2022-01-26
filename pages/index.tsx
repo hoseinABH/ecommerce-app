eb68b7b0b4f329cf9ee999391d3ec82370fa0516
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2022-01-26 12:54:00 +0330

    add another <Marquee /> with diffrent variant

--- pages/index.tsx
+++ pages/index.tsx
@@ -46,6 +46,12 @@ export default function Home({
           <ProductCard key={product.id} product={product} />
         ))}
       </Grid>
+
+      <Marquee variant="secondary">
+        {products.slice(0, 3).map((product) => (
+          <ProductCard key={product.id} variant="slim" product={product} />
+        ))}
+      </Marquee>
     </>
   );
 }

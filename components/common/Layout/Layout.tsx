fa1aa16e45cb9a91d01e08f643f0c4d7c3ead78e
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2022-01-26 12:58:25 +0330

    add <Footer /> component

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,10 +1,13 @@
 import { FC } from 'react';
+import Footer from '../Footer';
 import styles from './Layout.module.scss';
 
 const Layout: FC = ({ children }) => {
   return (
     <div className={styles.root}>
       <main className="fit">{children}</main>
+
+      <Footer />
     </div>
   );
 };
